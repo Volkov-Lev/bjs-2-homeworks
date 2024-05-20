42790770b3901171b548e7d40e59764f5655976a
Lev <03011984lev@gmail.com>
2024-05-20 14:24:21 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,12 +1,9 @@
-// Первая задача
 function getArrayParams(...arr) {
-  let min = Infinity; // Инициализация минимального значения как бесконечность
-  let max = -Infinity; // Инициализация максимального значения как отрицательная бесконечность
-  let sum = 0; // Инициализация суммы элементов
-  const len = arr.length; // Получение длины массива
-
-  // Проход по массиву и определение минимального, максимального и суммы элементов
-  for (let i = 0; i < len; i++) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
     if (element > max) {
       max = element;
@@ -16,18 +13,11 @@ function getArrayParams(...arr) {
     }
     sum += element;
   }
-
-  // Вычисление среднего арифметического
-  const avg = sum / len;
-  return { min, max, avg: avg.toFixed(2) }; // Возвращение объекта с результатами
+  
+  const avg = sum / arr.length;
+  return { min, max, avg };
 }
 
-// Примеры использования функции
-console.log(getArrayParams(-99, 99, 10)); // { min: -99, max: 99, avg: 3.33 }
-console.log(getArrayParams(1, 2, 3, -100, 10)); // { min: -100, max: 10, avg: -16.80 }
-console.log(getArrayParams(5)); // { min: 5, max: 5, avg: 5 }
-
-// Вторая задача
 function summElementsWorker(...arr) {
   return arr.reduce((acc, val) => acc + val);
 }
@@ -71,7 +61,6 @@ function averageEvenElementsWorker(...arr) {
   return countEven ? sumEven / countEven : 0;
 }
 
-// Третья задача
 function makeWork(arrOfArr, func) {
   let maxWorkerResult = -Infinity;
   arrOfArr.forEach(subArr => {
@@ -82,9 +71,3 @@ function makeWork(arrOfArr, func) {
   });
   return maxWorkerResult;
 }
-
-// Примеры использования функции
-const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
-console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
-console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
-console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 
