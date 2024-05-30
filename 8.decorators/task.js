e8206ee7068a77def82931a85dad9ec10d042478
Lev <03011984lev@gmail.com>
2024-05-30 18:31:35 +0300

    Initial commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,58 @@
+"use strict"
+
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(',')
+        let objectInCache = cache.findIndex((item) => item.hash === hash)
+        if (objectInCache !== -1) {
+            console.log("Из кэша: " + cache[objectInCache].value);
+            return "Из кэша: " + cache[objectInCache].value;
+        }
+        let result = func(...args);
+        cache.push({
+            hash: args.join(','),
+            value: result
+        })
+        if (cache.length > 5) {
+            cache.splice(0, 1);
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let checkFunc = false;
+    return function(...args) {
+        if (checkFunc == false) {
+            func(...args);
+            checkFunc = true;
+            setTimeout(() => {
+                checkFunc = false, func(...args)
+            }, delay);
+        }
+    }
+}
+
+function debounceDecorator2(func) {
+    let checkFunc = false;
+
+    function wrapper(...args) {
+        wrapper.count++;
+        if (checkFunc == false) {
+            func(...args);
+            checkFunc = true;
+            wrapper.count.push(i += 1)
+            setTimeout(() => {
+                checkFunc = false, func(...args)
+            }, delay);
+        }
+    }
+    wrapper.count = 0;
+    return wrapper
 }
