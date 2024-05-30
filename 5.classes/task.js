b96311a5804ad0928955730df97ef12383210b4e
Lev <03011984lev@gmail.com>
2024-05-30 17:54:09 +0300

    Initial commit

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,89 +1,98 @@
 class PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-      this.name = name;
-      this.releaseDate = releaseDate;
-      this.pagesCount = pagesCount;
-      this.state = 100; // Состояние по умолчанию
-      this.type = null; // Тип пока не определен
-    }
-  
-    fix() {
-      this.state *= 1.5;
-    }
-  
-    setState(newState) {
-      if (newState < 0) {
-        this.state = 0;
-      } else if (newState > 100) {
-        this.state = 100;
-      } else {
-        this.state = newState;
-      }
-    }
-  
-    get state() {
-      return this.state;
-    }
-  }
-  
-  class Magazine extends PrintEditionItem {
-    constructor(name, releaseDate, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-      this.type = 'magazine';
-    }
-  }
-  
-  class Book extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount, author) {
-      super(name, releaseDate, pagesCount);
-      this.author = author;
-      this.type = 'book';
-    }
-  }
-  
-  class NovelBook extends Book {
-    constructor(author, title, releaseYear, pagesCount) {
-      super(author, title, releaseYear, pagesCount);
-      this.type = 'novel';
-    }
-  }
-  
-  class FantasticBook extends Book {
-    constructor(author, title, releaseYear, pagesCount) {
-      super(author, title, releaseYear, pagesCount);
-      this.type = 'fantastic';
-    }
-  }
-  
-  class DetectiveBook extends Book {
-    constructor(author, title, releaseYear, pagesCount) {
-      super(author, title, releaseYear, pagesCount);
-      this.type = 'detective';
-    }
-  }
-  
-  // Примеры использования
-  const sherlock = new PrintEditionItem(
-    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-    2019,
-    1008
-  );
-  
-  console.log(sherlock.releaseDate); //2019
-  console.log(sherlock.state); //2019
-  console.log(sherlock.state); //100
-  sherlock.fix();
-  console.log(sherlock.state); //100
-  
-  const fantasticBook = new FantasticBook(
-    "Аркадий и Борис Стругацкие",
-    "Пикник на обочине",
-    1972,
-    168
-  );
-  
-  console.log(fantasticBook.author); //"Аркадий и Борис Стругацкие"
-  fantasticBook.state = 10;
-  console.log(fantasticBook.state); //10
-  fantasticBook.fix();
-  console.log(fantasticBook.state);
+	constructor(name, releaseDate, pagesCount, state, type) {
+	this.name = name;
+	this.releaseDate = releaseDate;
+	this.pagesCount = pagesCount;
+	this.state = 100;
+	this.type = null;
+	}
+
+	get state() {
+		return this._state;
+	}
+
+	set state(value) {
+		if (value < 0) {
+			this._state = 0;
+		} else if (value > 100) {
+			this._state = 100;
+		} else {
+			this._state = value;
+		}
+	}
+
+	fix() {
+	this.state = this.state * 1.5;
+	return this.state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(...args) {
+		super(...args);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author,...args) {
+		super(...args);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+class NovelBook extends Book {
+	constructor(...args) {
+		super(...args);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(...args) {
+		super(...args);
+		this.type = "fantastic";
+	}
+}
+	
+
+class DetectiveBook extends Book {
+	constructor(...args) {
+		super(...args);
+		this.type = "detective";
+	}
+}
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+
+	}
+
+	findBookBy(type, value) {
+		const item = this.books.find((book) => book[type] === value);
+		if (!item) {
+			return null;
+		}
+
+		return item;
+	}
+
+	giveBookByName(bookName) {
+		for (let i = 0; i < this.books.length; i++) {
+			if (this.books[i].name === bookName) {
+				return this.books.splice(i, 1)[0];
+			}
+		}
+
+		return null;
+	}
+}
