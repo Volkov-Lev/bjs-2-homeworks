502de6c294472d9ab5de0deb45706ff742fb6965
Lev <03011984lev@gmail.com>
2024-05-30 17:48:36 +0300

    Initial commit

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,53 +1,89 @@
-class Student {
-    constructor(name) {
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
       this.name = name;
-      this.grades = {}; // Словарь для хранения оценок по предметам
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100; // Состояние по умолчанию
+      this.type = null; // Тип пока не определен
     }
   
-    addMark(grade, subject) {
-      if (grade >= 1 && grade <= 5) {
-        if (!this.grades[subject]) {
-          this.grades[subject] = [];
-        }
-        this.grades[subject].push(grade);
+    fix() {
+      this.state *= 1.5;
+    }
+  
+    setState(newState) {
+      if (newState < 0) {
+        this.state = 0;
+      } else if (newState > 100) {
+        this.state = 100;
       } else {
-        console.error("Ошибка, оценка должна быть числом от 1 до 5");
+        this.state = newState;
       }
     }
   
-    getAverageBySubject(subject) {
-      const grades = this.grades[subject];
-      return grades ? grades.reduce((sum, grade) => sum + grade, 0) / grades.length : 0;
-   0;
-    }
-  
-    getAverage() {
-      let total = 0;
-      for (let subject in this.grades) {
-        total += this.getAverageBySubject(subject);
-   this.getAverageBySubject(subject);
-      }
-      return total / Object.keys(this.grades).length;
-    }
-  
-    exclude() {
-      console.log(`${this.name} исключен за попытку подделать оценки`);
-      // Здесь может быть логика для физического удаления студента из журнала
+    get state() {
+      return this.state;
     }
   }
   
-  // Пример использования
-  const student = new Student("Олег Никифоров");
-  student.addMark(5, "algebra");
-  student.addMark(5, "algebra");
-  student.addMark(5, "geometry");
-  student.addMark(4, "geometry");
-  student.addMark(6, "geometry"); // Ошибка, оценка должна быть числом от 1 до 5
-  console.error("Ошибка, оценка должна быть числом от 1 до 5");
-  student.addMark(5, "physics");
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.type = 'magazine';
+    }
+  }
   
-  console.log(student.getAverageBySubject("geometry")); // Средний балл по предмету geometry 4.5
-  console.log(student.getAverageBySubject("biology")); 4.5
-  console.log(student.getAverageBySubject("biology")); // Несуществующий предмет
-  console.log(student.getAverage()); // Средний балл по всем предметам 4.75
-  student.exclude();
+  class Book extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, author) {
+      super(name, releaseDate, pagesCount);
+      this.author = author;
+      this.type = 'book';
+    }
+  }
+  
+  class NovelBook extends Book {
+    constructor(author, title, releaseYear, pagesCount) {
+      super(author, title, releaseYear, pagesCount);
+      this.type = 'novel';
+    }
+  }
+  
+  class FantasticBook extends Book {
+    constructor(author, title, releaseYear, pagesCount) {
+      super(author, title, releaseYear, pagesCount);
+      this.type = 'fantastic';
+    }
+  }
+  
+  class DetectiveBook extends Book {
+    constructor(author, title, releaseYear, pagesCount) {
+      super(author, title, releaseYear, pagesCount);
+      this.type = 'detective';
+    }
+  }
+  
+  // Примеры использования
+  const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  );
+  
+  console.log(sherlock.releaseDate); //2019
+  console.log(sherlock.state); //2019
+  console.log(sherlock.state); //100
+  sherlock.fix();
+  console.log(sherlock.state); //100
+  
+  const fantasticBook = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  );
+  
+  console.log(fantasticBook.author); //"Аркадий и Борис Стругацкие"
+  fantasticBook.state = 10;
+  console.log(fantasticBook.state); //10
+  fantasticBook.fix();
+  console.log(fantasticBook.state);
