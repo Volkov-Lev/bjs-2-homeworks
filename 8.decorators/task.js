a5dded5b5dcec7799e1bf31e0a392cba37bad2f1
Lev <03011984lev@gmail.com>
2024-05-30 18:57:49 +0300

    Initial commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -34,19 +34,6 @@ function cachingDecoratorNew(func) {
   return wrapper;
 }
 
-// Примеры использования
-const addAndMultiply = (a, b, c) => (a + b) * c;
-const upgraded = cachingDecoratorNew(addAndMultiply);
-
-upgraded(1, 2, 3); // вычисляем: 9
-upgraded(1, 2, 3); // из кеша: 9
-upgraded(2, 2, 3); // вычисляем: 12
-upgraded(3, 2, 3); // вычисляем: 15
-upgraded(4, 2, 3); // вычисляем: 18
-upgraded(5, 2, 3); // вычисляем: 21
-upgraded(6, 2, 3); // вычисляем: 24 (при этом кеш для 1, 2, 3 уничтожается)
-upgraded(1, 2, 3); // вычисляем: 9  (снова вычисляем, кеша нет)
-
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
     let timeoutId;
