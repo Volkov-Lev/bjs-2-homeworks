dc13cae5e79ae6630052f915ee58d069db824c79
Lev <03011984lev@gmail.com>
2024-06-15 17:19:07 +0300

    «Декораторы» задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,44 +1,42 @@
-//Задача № 1
+///Задача № 1
 function cachingDecoratorNew(func) {
   let cache = [];
-
   function wrapper(...args) {
-    const hash = args.join(',');
-    let idx = cache.findIndex((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
-    if (idx !== -1) { // если элемент не найден
-      console.log("Из кэша: " + cache[idx].value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-      return "Из кэша: " + cache[idx].value;
-    } else {
-      let result = func(...args); // в кэше результата нет - придётся считать
-      cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
-      if (cache.length > 5) {
-        cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый) 
-      }
-      console.log("Вычисляем: " + result);
-      return "Вычисляем: " + result;
+    const hash = md5(args);
+    let objectInCache = cache.find( item => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
     }
+    let result = func(...args);
+    cache.push({"hash": hash, "value": result});
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
   }
   return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-    let timeoutId;
-    let isTrottled = false;
-    function wrapper(...args) {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout( () => {
-        func(args);
-        wrapper.count++;
-      }, delay);
-      if (!isTrottled) {
-        func(...args);
-        wrapper.count++;
-        isTrottled = true;
-      }
-      wrapper.allCount++;
+  let timeoutId;
+  let isTrottled = false;
+  function wrapper(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout( () => {
+      func(args);
+      wrapper.count++;
+    }, delay);
+    if (!isTrottled) {
+      func(...args);
+      wrapper.count++;
+      isTrottled = true;
     }
-    wrapper.count = 0;
-    wrapper.allCount = 0;
-    return wrapper;
+    wrapper.allCount++;
   }
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  return wrapper;
+}
