b5314ccabb60b16ea20615712f36d497a9fede56
Lev <03011984lev@gmail.com>
2024-06-25 15:46:34 +0300

    «Асинхронность» задание полностью готово

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,58 +1,63 @@
-'use strict'
+'use strict';
 
 class AlarmClock {
     constructor() {
-      this.alarmCollection = [];
-      this.intervalId = null;
+        this.alarmCollection = [];
+        this.intervalId = null;
     }
-  
+
     addClock(time, callback) {
-      if (!time || !callback) {
-        throw new Error('Отсутствуют обязательные аргументы');
-      }
-      if (this.alarmCollection.find(alarm => alarm.time === time)) {
-        console.warn('Уже присутствует звонок на это же время');
-      }
-      this.alarmCollection.push({time, callback, canCall: true});
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        if (this.alarmCollection.find(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+        this.alarmCollection.push({time, callback, canCall: true});
     }
-  
+
     removeClock(time) {
-      this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
     }
-  
+
     getCurrentFormattedTime() {
-        //return new Date().toLocaleTimeString().slice(0, -3);
         const currentTime = new Date().toLocaleTimeString("ru-Ru", {
-          hour: "2-digit",
-          minute: "2-digit",
-      });
-      return currentTime.slice(0, -3);
-  }
-    
-  
-    start() {
-      if (this.intervalId !== null) {
-        return;
-      }
-      this.intervalId = setInterval(() => {
-        this.alarmCollection.forEach(alarm => {
-          if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
-            alarm.canCall = false;
-            alarm.callback();
-          }
+            hour: "2-digit",
+            minute: "2-digit",
         });
-      }, 1000);
+        return currentTime.slice(0, -3);
     }
-  
+
+    start() {
+        if (this.intervalId !== null) {
+            return;
+        }
+        this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        }, 1000);
+    }
+
     stop() {
         clearInterval(this.intervalId);
         this.intervalId = null;
-      }
-      resetAllCalls() {
-        this.alarmCollection.forEach( setup => setup.canCall = true);
-      }
-      clearAlarms() {
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(setup => setup.canCall = true);
+    }
+
+    clearAlarms() {
         this.stop();
         this.alarmCollection = [];
-      }
     }
+}
+
+// Пример использования класса AlarmClock:
+const clock = new AlarmClock();
+clock.addClock('06:30', () => console.log('Звонок будильника'));
+clock.start();
